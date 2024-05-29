41330e0a8ab4e5c902e95489f81c5f5cae074610
jmkim <jmkim0117@riskzero.kr>
2024-05-29 14:46:20 +0900

    init project

--- src/main/kotlin/com/namejm/gitToSlack/controller/HomeController.kt
+++ src/main/kotlin/com/namejm/gitToSlack/controller/HomeController.kt
@@ -2,18 +2,35 @@ package com.namejm.gitToSlack.controller
 
 import com.namejm.gitToSlack.dto.RestResult
 import jakarta.servlet.http.HttpServletRequest
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 
 
 @Controller
 class HomeController {
 
-    @GetMapping(value = ["/request"])
-    fun root(request: HttpServletRequest): ResponseEntity<RestResult> {
+    @Value("\${app.secret}")
+    lateinit var secret: String
+
+    @PostMapping(value = ["/request"])
+    fun root(@RequestBody param: Map<String, Object>, request: HttpServletRequest): ResponseEntity<RestResult> {
         val result = RestResult()
+
+        val headerSecret = request.getHeader("x-ncp-sourcecommit-signature-v1")
+
+        println("[Request Secret Header]")
+        println("secret: $secret")
+        println("headerSecret: $headerSecret")
+
+        println("[Request Body]")
+        println(param.map { "${it.key}: ${it.value}" }.joinToString(", "))
+
+
+
 
         result.success = true
         return ResponseEntity(result, HttpStatus.OK)
